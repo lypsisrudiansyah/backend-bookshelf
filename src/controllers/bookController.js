1c4f217b3c1275c50c55ed0222292faf446b1517
rudiansyah <rudiansyahether@gmail.com>
2025-10-19 12:57:55 +0700

    ✨ (bookController.js): add addBook and updateBook controllers to handle book creation and update

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -25,3 +25,82 @@ exports.getBookById = (req, res) => {
     data: { book },
   });
 };
+
+exports.addBook = (req, res) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = req.body;
+
+  // validating according our rules
+  if (!name) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+  }
+
+  if (readPage > pageCount) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+  }
+
+  const bookId = bookModel.addBook({
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Buku berhasil ditambahkan',
+    data: { bookId },
+  });
+};
+
+exports.updateBook = (req, res) => {
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = req.body;
+
+  if (!name) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+  }
+
+  if (readPage > pageCount) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+  }
+
+  const updated = bookModel.updateBook(req.params.id, {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  });
+
+  if (!updated) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+  }
+
+  res.json({
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  });
+};
+
+

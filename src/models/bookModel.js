472dbf04def56e1d2b3a0fda71a8e364f0ceaa8a
rudiansyah <rudiansyahether@gmail.com>
2025-10-19 12:20:02 +0700

    ✨ (bookModel.js): add addBook function to add a new book to the books array

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -11,3 +11,27 @@ function getBookById(id) {
   return books.find(b => b.id === id);
 }
 
+function addBook(data) {
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = data.pageCount === data.readPage;
+
+  const newBook = {
+    id,
+    name: data.name,
+    year: data.year,
+    author: data.author,
+    summary: data.summary,
+    publisher: data.publisher,
+    pageCount: data.pageCount,
+    readPage: data.readPage,
+    finished,
+    reading: data.reading,
+    insertedAt,
+    updatedAt,
+  };
+
+  books.push(newBook);
+  return id;
+}
